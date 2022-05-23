f57d3e57cc2aba03aae08e5b127c228f3fc6713a
arufxuenzaku <37031330+arufxuenzaku@users.noreply.github.com>
2022-05-24 01:00:34 +0700

    Update _anti_boom_click.js

--- theme/apmody/js/_anti_boom_click.js
+++ theme/apmody/js/_anti_boom_click.js
@@ -3,12 +3,12 @@
 if("undefined"!=typeof xAu){
 var clsAd,clsiAd,mxcAd,rstAd,nmcAd,ntwAd;
 
-clsAd=null==ARtb.antiBoomAd.classAd?".adsbygoogle, .adB":ARtb.antiBoomAd.classAd;
-clsiAd=null==ARtb.antiBoomAd.iframeAd?".adsbygoogle iframe":ARtb.antiBoomAd.iframeAd;
-mxcAd=null==ARtb.antiBoomAd.maxClick?3:ARtb.antiBoomAd.maxClick;
-rstAd=null==ARtb.antiBoomAd.resetTime?3600:ARtb.antiBoomAd.resetTime;
-nmcAd=null==ARtb.antiBoomAd.name?"AD_Click":ARtb.antiBoomAd.name;
-ntwAd=null==ARtb.antiBoomAd.msg?"Please don't be naughty if you don't want to support us..":ARtb.antiBoomAd.msg;
+clsAd=null==ARtb.antiBoomClick.classAd?".adsbygoogle, .adB":ARtb.antiBoomClick.classAd;
+clsiAd=null==ARtb.antiBoomClick.iframeAd?".adsbygoogle iframe":ARtb.antiBoomClick.iframeAd;
+mxcAd=null==ARtb.antiBoomClick.max?3:ARtb.antiBoomClick.max;
+rstAd=null==ARtb.antiBoomClick.resetTime?3600:ARtb.antiBoomClick.resetTime;
+nmcAd=null==ARtb.antiBoomClick.name?"AD_Click":ARtb.antiBoomClick.name;
+ntwAd=null==ARtb.antiBoomClick.msg?"Please don't be naughty if you don't want to support us..":ARtb.antiBoomClick.msg;
 
 function dsbAD(){var n=qSell(clsAd);if(n.length>0)for(var t=0;t<n.length;t++)n[t].style.pointerEvents="none"}
 var clcAd=xAR.gC(nmcAd);null!=clcAd&&mxcAd==clcAd&&dsbAD(),qSell(clsAd).forEach((A=>{A.addEventListener("click",(A=>{if(null==xAR.gC(nmcAd))xAR.sC(nmcAd,1,{secure:!0,"max-age":rstAd});else{var c=Number(xAR.gC(nmcAd));if(c<mxcAd){var d=c+1;xAR.sC(nmcAd,d,{secure:!0,"max-age":rstAd}),d==mxcAd&&(getid("tNtf").innerHTML="<span>"+ntwAd+"</span>",dsbAD())}else A.preventDefault()}}))}));
